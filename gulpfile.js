435641d0abbf1a2e9abdad5eee9c6f924993df4d
jrholden <jholden@upei.ca>
2018-07-03 12:22:38 -0300

    created pages, playing around with differnt views | grunt | scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,97 @@
 
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var minifyHtml = require('gulp-minify-html');
-var angularTemplatecache = require('gulp-angular-templatecache');
-var useref = require('gulp-useref');
-var ngAnnotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var del = require('del');
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+let sass        = require('gulp-sass');
+/*let jshint = require('gulp-jshint');
+let jscs = require('gulp-jscs');
+let rename = require('gulp-rename');
+let babel = require('gulp-babel');
+let minifyHtml = require('gulp-minify-html');
+let angularTemplatecache = require('gulp-angular-templatecache');
+let useref = require('gulp-useref');
+let ngAnnotate = require('gulp-ng-annotate');
+let uglify = require('gulp-uglify');*/
+let concat = require('gulp-concat');
+let del = require('del');
 
-var config = {
-    js: 'js/**/*.js',
-    images: 'images/*.*',
-    fonts: 'fonts/*.*',
-    html: 'templates/*.html',
-    temp: 'temp/'
+
+const paths = {
+    src: 'src',
+    styles: {
+        src: ['node_modules/bootstrap/scss/**', 'src/scss/*.scss'],
+        dist: 'dist/css'
+    },
+    scripts: {
+        src: ['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js','node_modules/tether/dist/js/tether.min.js', 'node_modules/angular/angular.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/angular-route/angular-route.min.js'],
+        dist: 'dist/js'
+    },
+    server: {
+        baseDir: '/xampp/htdocs/SouthportDental'
+    },
+    dist: {
+        path: 'dist'
+    }
 };
 
-var dist = {
-    path: 'src/',
-    images: 'images/',
-    fonts: 'fonts/'
-};
 
+const server = browserSync.create();
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+function reload(done) {
 
-// Move the javascript files into our /src/js folder
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js','node_modules/tether/dist/js/tether.min.js', 'node_modules/angular/angular.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/angular-route/angular-route.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
-gulp.task('fonts', function() {
-    return gulp.src(['node_modules/fonts/**'])
-        .pipe(gulp.dest("src/fonts"))
-        .pipe(browserSync.stream());
-});
+    server.reload();
+    done();
+}
 
-gulp.task('deploy', ['sass'], function () {
-    return gulp.src(['src/**'])
-        .pipe(gulp.dest('/xampp/htdocs/SouthportDental'))
-        .pipe(browserSync.stream());
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['deploy'], function() {
-
-    browserSync.init({
-        server: "/xampp/htdocs/SouthportDental"
+function serve(done) {
+    server.init({
+        files: [paths.src + '/**'],
+        server: {
+            baseDir: paths.dist.path
+        }
     });
-
-    gulp.watch(['src/**/*'], ['deploy']);
-    gulp.watch("src/**").on('change', browserSync.reload);
-});
+    done();
+}
 
 
-gulp.task('default', ['js','sass','fonts']);
+function clean() {
+    return del([paths.scripts.dist, paths.styles.dist]);
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(concat('main.min.css'))
+        .pipe(gulp.dest(paths.styles.dist))
+}
+
+function scripts() {
+    return gulp.src(paths.scripts.src)
+
+        .pipe(gulp.dest(paths.scripts.dist));
+}
+
+function watch() {
+    gulp.watch(paths.scripts.src, gulp.series([ reload]));
+    gulp.watch(paths.styles.src, gulp.series([styles, reload]));
+    gulp.watch([paths.src+'/*.html',paths.src+'/**/*.html'], gulp.series([clean,styles,scripts,stage, reload]));
+}
+
+function stage() {
+    return gulp.src([paths.src+'/**'])
+        .pipe(gulp.dest(paths.dist.path))
+}
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.stage = stage;
+exports.reload = reload;
+exports.serve = serve;
+
+let build = gulp.series(clean, gulp.parallel(styles, scripts), stage, serve, watch);
+
+gulp.task('build', build);
+
+gulp.task('default', build);
